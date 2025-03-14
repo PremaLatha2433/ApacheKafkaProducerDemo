b55fa88cd8e14f41bca3d20148f45b4956d3453a
premalathamunigela <premalatha.munigela@mclaneco.com>
2025-03-14 21:49:24 +0530

    initial commit

---
+++ src/main/java/com/apache/kafka/controller/EventController.java
@@ -0,0 +1,31 @@
+package com.apache.kafka.controller;
+
+import com.apache.kafka.services.KafkaMessagePublisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/producer-app")
+public class EventController {
+
+    @Autowired
+    private KafkaMessagePublisher kafkaMessagePublisher;
+
+
+    @GetMapping("/publish/{message}")
+    public ResponseEntity<?> publishMessage(@PathVariable String message){
+        try {
+            for(int i=0;i<10000;i++) {
+                kafkaMessagePublisher.sendMessageToTopic(message+":"+i);
+            }
+            return new ResponseEntity<>("message published sucessfully..", HttpStatus.OK);
+        }catch (Exception e){
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
---
+++ src/main/java/com/apache/kafka/services/KafkaMessagePublisher.java
@@ -0,0 +1,26 @@
+package com.apache.kafka.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+
+    @Autowired
+    private KafkaTemplate<String,Object> template;
+
+    public void sendMessageToTopic(String message){
+      CompletableFuture<SendResult<String,Object>> future = template.send("premaBeHappy",message);
+    future.whenComplete((result,ex)->{
+    if (ex == null){
+        System.out.println("sent message=["+message+" ] with offset =["+result.getRecordMetadata().offset()+"]");
+    }else{
+        System.out.println("Unable to send message =["+message+"] due to : "+ex.getMessage());
+    }
+    });
+    }
+}
